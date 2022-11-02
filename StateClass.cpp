ef596401e2c3b6e9a86494abe5f4c27405b5f62c
harshvardhan-sharma1 <harshanand002@gmail.com>
2022-11-01 18:12:26 -0700

    Testing the misplaced Tile function
    passed tests

--- StateClass.cpp
+++ StateClass.cpp
@@ -119,9 +119,11 @@ class State
   int misplacedTile(State* x) 
   {
     int counter = 0;
-    for(unsigned i =0; i< x->currState.size(); i++)
+    if(x->currState.back()!=0)
+      counter++;
+    for(unsigned i =0; i< x->currState.size()-1; i++)
     {
-      if(x->currState.at(i) != i)
+      if(x->currState.at(i) != i+1)
         counter++;
     }
     return counter;
@@ -140,37 +142,45 @@ int main()
     {
         v.push_back(i+1);
     }
+    v.push_back(0);
     for(int i=7; i>=0; i--)
     {
         v2.push_back(i+1);
     }
+    v2.push_back(0);
 
     State *curr = new State(v);
+    cout << "\nC2:\n";
     curr->display();
-
     cout << "\n\n\n";
+
+
     State *c = new State(v2);
-    curr = c;
-    curr->display();
+    cout << "\nC1:\n";
+    c->display();
+    cout << "\n\n\n";
 
-    cout << "\n\nV2 == V ?? --> " << (v2==v) << " | V2!=V ?? --> " << (v2!=v) << "\n";
+    // cout << "\n\nV2 == V ?? --> " << (v2==v) << " | V2!=V ?? --> " << (v2!=v) << "\n";
     
-    State *c3 = new State();
+    State *c3 = new State(v2);
+    cout << "\nC3:\n";
     c3->display();
+    cout << "\n\n\n";
 
-    cout << "IS C1 = goal? " << c->isGoal() << endl;
-    cout << "IS C3 = goal? " << c3->isGoal() << endl << endl;
+    // cout << "IS C1 = goal? " << c->isGoal() << endl;
+    // cout << "IS C3 = goal? " << c3->isGoal() << endl << endl;
     
     for(unsigned i=0; i<8; i++)
     {
       v.at(i) = i+1;
     }
+    v.at(v.size()-1) = 0;
     State *c4 = new State(v);
     c4->display();
     cout << "Misplaced tile C1: " <<c->misplacedTile(c) << endl;
-    cout << "Misplaced tile C1: " <<curr->misplacedTile(curr) << endl;
-    cout << "Misplaced tile C1: " <<c3->misplacedTile(c3) << endl;
-    cout << "Misplaced tile C1: " <<c4->misplacedTile(c4) << endl;
+    cout << "Misplaced tile C2: " <<curr->misplacedTile(curr) << endl;
+    cout << "Misplaced tile C3: " <<c3->misplacedTile(c3) << endl;
+    cout << "Misplaced tile C4: " <<c4->misplacedTile(c4) << endl;
 
    return 0;
 }
